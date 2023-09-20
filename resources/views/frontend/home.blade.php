87e88dac3fb2adc559ebf090026697ab1ecaa099
Ayman Ait Laaguid <laaguid.ayman@gmail.com>
2023-09-20 12:41:35 +0100

    Home Laravelisation

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -1,4 +1,10 @@
 @extends('frontend.layouts.layout')
 @section('content')
     @include('frontend.partials.banner')
+    @include('frontend.partials.featured')
+    @include('frontend.partials.product_list')
+    @include('frontend.partials.sale')
+    @include('frontend.partials.best_seller')
+    @include('frontend.partials.subscribe')
+    @include('frontend.partials.clients')
 @endsection
